0112c070d687d962859b564037cf1b8032e911b3
Grzegorz Wolanski <grzegorz.wolanski4321@wp.pl>
2019-12-29 00:39:41 +0100

    Attaching IOhandler to main.cpp

--- Menu/IOhandler.cpp
+++ Menu/IOhandler.cpp
@@ -28,36 +28,49 @@ bool IOhandler::validateInput()
 {
     switch(currentSelection)
     {
+        case '0':
+        {
+            std::cout << "Wybrano " << currentSelection << std::endl;
+        }
+        break;
         case '1':
         {
+            std::cout << "Wybrano " << currentSelection << std::endl;
         }
         break;
         case '2':
         {
+            std::cout << "Wybrano " << currentSelection << std::endl;
         }
         break;
         case '3':
         {
+            std::cout << "Wybrano " << currentSelection << std::endl;
         }
         break;
         case '4':
         {
+            std::cout << "Wybrano " << currentSelection << std::endl;
         }
         break;
         case '5':
         {
+            std::cout << "Wybrano " << currentSelection << std::endl;
         }
         break;
         case '6':
         {
+            std::cout << "Wybrano " << currentSelection << std::endl;
         }
         break;
         case '7':
         {
+            std::cout << "Wybrano " << currentSelection << std::endl;
         }
         break;
         default:
         {
+            std::cout << "Wybrano " << currentSelection << std::endl;
             std::cout << "Wybrana opcja nie istnieje." << std::endl << std::endl;
             return false;
         }
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <Miscellanous/Timer.hpp>
+#include <Menu/IOhandler.hpp>
 #include <Miscellanous/ParametricArray.hpp>
 #include <Graph/ArrayGraph.hpp>
 #include <Graph/ListGraph.hpp>
@@ -24,7 +25,26 @@ const bool useListGraph = false;
 
 int main()
 {
-    Timer timer;                // czasomierz
+    Timer timer;
+    IOhandler ioHandler(std::string("PEA Projekt nr 1: Travelling Salesman Problem\n") +
+                        std::string("Autor: Grzegorz Wolanski\n\n") +
+                        std::string("Menu:\n") +
+                        std::string("1. Wczytaj graf z pliku.\n") +
+                        std::string("2. Wygeneruj losowy graf.\n") +
+                        std::string("3. Wyświetl graf.\n") +
+                        std::string("4. Wykonaj przegląd zupełny.\n") +
+                        std::string("5. Wykonaj algorytm podzialu i ograniczen.\n") +
+                        std::string("6. Testy dla przegladu zupelnego.\n") +
+                        std::string("7. Testy dla Branch & Bound.\n") +
+                        std::string("0. Wyjscie.\n\n") +
+                        std::string("Wybor: "));
+
+    while(ioHandler.getCurrentMenuSelection() != '0')
+    {
+        ioHandler.printMenu();
+        ioHandler.readMenuSelection();
+    }
+
     char menuSelection;
     Graph *graph = NULL;            // <- tu bedziemy zapisywac adresy przez caly program
 
